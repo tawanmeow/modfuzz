81836d3e34e89ce2d83190a2df2d6c216e1110fd
tawanmeow <hh.tawann@gmail.com>
2022-02-02 15:09:04 +0700

    update Galil-dos script

--- call_galil.py
+++ call_galil.py
@@ -2,7 +2,7 @@ import os
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 APPFILE = os.path.join(CURRENT_DIRECTORY, "galil.py")
 
-ATTEMPT = 10
+ATTEMPT = 100
 for i in range(0, ATTEMPT):
     os.system('python ' + APPFILE)
     print(str(i + 1))
